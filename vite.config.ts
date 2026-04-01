9c2d633b5d707a8a3a88e0b0cb82e4c91d8ce50a
Nicholas <nigarofe@gmail.com>
2026-04-01 14:53:06 -0300

    working frontpage

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    root: 'src',
+    publicDir: '../public',
     server: {
         proxy: {
-            '/': {
+            '/svgs': {
+                target: 'http://localhost:3010',
+                changeOrigin: true,
+            },
+            '/events': {
+                target: 'http://localhost:3010',
+                changeOrigin: true,
+            },
+            '/files': {
+                target: 'http://localhost:3010',
+                changeOrigin: true,
+            },
+            '/assets': {
                 target: 'http://localhost:3010',
                 changeOrigin: true,
             },
